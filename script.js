91c75a2f2e2f594c7fc1b6de16aece92526da68f
valeed <codewithvaleed.com>
2023-05-17 00:15:39 +0530

    single product page added

--- script.js
+++ script.js
@@ -12,3 +12,23 @@ if (close) {
         nav.classList.remove('show-nav');
     })
 }
+
+
+// single product page image slider
+
+
+let mainimg = document.getElementById("mainImg");
+let smallimg = document.getElementsByClassName("small-img");
+
+smallimg[0].onclick = function () {
+    mainimg.src = smallimg[0].src;
+}
+smallimg[1].onclick = function () {
+    mainimg.src = smallimg[1].src;
+}
+smallimg[2].onclick = function () {
+    mainimg.src = smallimg[2].src;
+}
+smallimg[3].onclick = function () {
+    mainimg.src = smallimg[3].src;
+}
